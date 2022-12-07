4ddae64da1e434732e3fd2fd356489ab36cc0a9f
wujiandong <18007002487@163.com>
2022-12-07 09:10:23 +0800

    修改业务状态枚举值

--- common/src/main/java/com/supply/common/constant/BusinessStatusEnum.java
+++ common/src/main/java/com/supply/common/constant/BusinessStatusEnum.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 
 /**
  * @author wjd
- * @description 业务状态--每个模块准备199个序号.
+ * @description 业务状态--每个模块准备200个序号.
  * @date 2022-07-28
  */
 @Getter
@@ -19,13 +19,13 @@ public enum BusinessStatusEnum {
 
     IN_EXPIRE(3, "过期", ModuleEnum.COMMON),
 
-    WAIT_RELATION(200, "待关联", ModuleEnum.FILE),
+    WAIT_RELATION(201, "待关联", ModuleEnum.FILE),
 
-    RELATION(201, "已关联", ModuleEnum.FILE),
+    RELATION(202, "已关联", ModuleEnum.FILE),
 
-    UN_READER(400, "未读", ModuleEnum.MESSAGE),
+    UN_READER(401, "未读", ModuleEnum.MESSAGE),
 
-    READER(401, "已读", ModuleEnum.MESSAGE);
+    READER(402, "已读", ModuleEnum.MESSAGE);
 
 
     @Note(description = "业务状态")
