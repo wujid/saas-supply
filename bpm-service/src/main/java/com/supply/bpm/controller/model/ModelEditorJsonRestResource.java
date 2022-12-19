bb352092d1ef35c863e3e2cbb174a94b8d9e0e6f
wujid <46591102+wujid@users.noreply.github.com>
2022-12-19 18:18:26 +0800

    调整位置

--- bpm-service/src/main/java/com/supply/bpm/controller/model/ModelEditorJsonRestResource.java
+++ bpm-service/src/main/java/com/supply/bpm/controller/model/ModelEditorJsonRestResource.java
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.supply.bpm.config;
+package com.supply.bpm.controller.model;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
