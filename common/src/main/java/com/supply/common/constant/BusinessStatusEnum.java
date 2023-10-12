92e57c0cb336be2cba8d366599979c5cce7c4649
wujiandong <18007002487@163.com>
2023-10-12 17:13:42 +0800

    修改枚举值名称

--- common/src/main/java/com/supply/common/constant/BusinessStatusEnum.java
+++ common/src/main/java/com/supply/common/constant/BusinessStatusEnum.java
@@ -35,7 +35,7 @@ public enum BusinessStatusEnum {
 
     PROCESS_STATUS_SUSPEND(704, "流程挂起", ModuleEnum.BPM),
 
-    PROCESS_STATUS_END(705, "流程结束", ModuleEnum.BPM);
+    PROCESS_STATUS_END(705, "流程完成", ModuleEnum.BPM);
 
 
 
