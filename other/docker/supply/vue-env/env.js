81325dfd5db4f932010ff5f0a2a1e9e8332dd490
wujiandong <18007002487@163.com>
2023-10-17 18:00:52 +0800

    修改部署信息

---
+++ other/docker/supply/vue-env/env.js
@@ -0,0 +1,15 @@
+window.webConfig = {
+  // 网站名称
+  system_name: '云梦成美',
+  // 统一网关
+  gateway: 'https://api.saascloud.vip',
+  // webSocket前缀
+  ws_schema: 'wss',
+  // xx-job
+  xx_job: 'https://job.saascloud.vip/job',
+  // icon
+  system_icon: '',
+  // logo
+  system_logo: ''
+
+}
